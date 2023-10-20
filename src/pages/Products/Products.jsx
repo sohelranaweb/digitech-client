dba7ee1ebdc5a452329c79a35b416f9617c10672
sohelranaweb <sohel.rana.web2@gmail.com>
2023-10-20 18:26:27 +0600

    slider added

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -7,8 +7,54 @@ const Products = () => {
 
   console.log(prodcuts);
   return (
-    <div>
+    <div className="lg:px-0 px-3">
       <Navbar></Navbar>
+      <div className="mt-10">
+        <div className="carousel w-full lg:h-[600px] h-[400px]">
+          <div id="slide2" className="carousel-item relative w-full">
+            <img
+              src="https://i.ibb.co/qpnrdDD/adv3-2-200.webp"
+              className="w-full"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full">
+            <img
+              src="https://i.ibb.co/wp4hhz5/adv2-2-200.webp"
+              className="w-full"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide4" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide4" className="carousel-item relative w-full">
+            <img
+              src="https://i.ibb.co/WtFXFg5/adv1-1-200.webp"
+              className="w-full"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide3" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide1" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="grid lg:grid-cols-2 grid-cols-1 gap-6 mt-20 lg:px-0 px-3">
         {prodcuts.map((product) => (
           <ProductCard key={product._id} product={product}></ProductCard>
